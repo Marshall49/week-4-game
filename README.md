617bc95f1c8119862c29a0431de614dda4a6e564
Malcolm Strong <MalcolmStrong@Malcolms-MBP.attlocal.net>
2017-05-21 19:34:36 -0400

    edited javascript

---
+++ README.md
@@ -0,0 +1,20 @@
+We are gonna psuedocode here.
+
+The point of the game is:
+we are clicking on 4 different teams, each with a new random numerical value, and we want to make the values that we are adding together exactly equal to another random number. 
+
+1. The number that we wanna reach.
+	-gonna be random every time the game starts
+	
+2. A display of the wins and losses.
+
+3. The teams and their random values.
+	- 4 teams
+	-each has a random value
+	-every time you click one, that value gets added to the 'score'
+	-every time the game starts, the teams get a new random value
+
+4. The number that gets updated every time we click a teams.
+
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+	//numbers
+	
+	//these are all going to get a random value at the START of each game
+	var targetScore;
+
+	var ugaNumber;
+	var falconsNumber;
+	var gaTechNumber;
+	var hawksNumber;
+
+	//wins and losses get updated at the END of each game
+
+	var wins = 0;
+	var losses = 0;
+
+	//the user's current score gets updated throughout the game, specifically every time you click on a team
+	var currentScore;
+	//there will be a conditional (if) statement that checks the value of currentScore compared to targetScore
+	//if that value is lower, the game continues
+	//if the value is HIGHER, the game is over, and we add to losses
+	//if the value is EQUAL, the game is over, and we add to wins
+
+	function gameStart() {
+
+	targetScore = Math.floor(Math.random() * 100);
+	$("#guessingnumberbox").html(targetScore);
+	console.log(targetScore);
+
+	currentScore = 0;
+	$("#total-score").html(currentScore);
+
+	ugaNumber = (Math.floor(Math.random() * 10)) +1;
+	gaTechNumber = (Math.floor(Math.random() * 10)) +1;
+	falconsNumber = (Math.floor(Math.random() * 10)) +1;
+	hawksNumber = (Math.floor(Math.random() * 10)) +1;
+	console.log(ugaNumber);
+	console.log(gaTechNumber);
+	console.log(falconsNumber);
+	console.log(hawksNumber);
+
+	}
+
+	gameStart();
+
+	//function that checks to see if the game is over:
+	function checkGameStatus () {
+		if (currentScore = targetScore) {
+			wins++;
+			// some other stuff happens, aka we either start a new game, display "you win!", etc;
+		} else if (currentScore > targetScore) {
+			losses++;
+			// some other stuff happens, start a new game, display "you lose!", etc.
+		} else { return }
+
+	}
+		
+	function takingAturn () {
+	//middle of the game, taking a 'turn':
+	$("#falcons").on("click", function () {
+		currentScore = currentScore + falconsNumber;
+		checkGameStatus();
+	});
+	
+	$("#uga").on("click", function () {
+		currentScore = currentScore + ugaNumber;
+		checkGameStatus();
+	});
+
+	$("#hawks").on("click", function () {
+		currentScore = currentScore + hawksNumber;
+		checkGameStatus();
+	});
+
+	$("#gaTech").on("click", function () {
+		currentScore = currentScore + gaTechNumber;
+		checkGameStatus();
+	});
+	
+
+	
+
+
+
+
+
+
+
+	// ^^^make another on of these for each logo;
+	// then figure out what else happens when the game is over;
+
+
+
+
+
+
+
+	// function newTeams () {
+	// 	var numbers = [];
+	// }
+
+	// function newGame() {
+
+	// }
+
+
+
+	// $('#win').text(wins);
+	// $('#loss').text(losses);
+
+// console.log("Hello from game.js")
