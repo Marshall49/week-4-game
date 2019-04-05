74ec03c8830b1e2ee2e88f3728c31b35a71b44a0
Malcolm Strong <strong.malcolm@yahoo.com>
2019-04-04 22:33:06 -0400

    editing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
         }
     }
 
-    	
+
 
     	function takingAturn () {
 	// middle of the game, taking a 'turn':
@@ -71,7 +71,7 @@ $(document).ready(function() {
         $("#total-score").html(currentScore);
         addTotal();
 	});
-	
+
 	 $("#uga").on("click", function() {
         currentScore = currentScore + ugaNumber;
         // resetValues();
@@ -85,7 +85,7 @@ $(document).ready(function() {
         $("#total-score").html(currentScore);
         addTotal();
     });
-	
+
 		$("#hawks").on("click", function() {
         currentScore = currentScore + hawksNumber;
         // resetValues();
@@ -95,244 +95,3 @@ $(document).ready(function() {
 	}
 
 	takingAturn();
-
-
-
-
-
-
-
-
-
-
-
-	//function that checks to see if the game is over:
-	// function checkGameStatus () {
-	// 	if (currentScore = targetScore) {
-	// 		$('#scorebox').text('You won!!!!');
-	// 		wins++;
-	// 		// some other stuff happens, aka we either start a new game, display "you win!", etc;
-	// 	} else if (currentScore > targetScore) {
-	// 		$('#scorebox').text('You lose!!!!');
-	// 		losses++;
-	// 		// some other stuff happens, start a new game, display "you lose!", etc.
-	// 	} else { return }
-
-	// }
-
-	// checkGameStatus();
-		
-	
-
-		 // function addTotal(teamValue){
-   //      targetScore = currentScore + currentScore;
-   //      $("#total-score").html(targetScore);
-   //      if (currentScore > targetScore){
-   //          //LOSING
-   //          console.log("LOST");
-   //          losses = losses + 1;
-   //          $("#losses").html("Losses: " + losses);
-   //          resetValues();
-   //      } else if (targetScore == targetScore){
-   //          console.log("WIN");
-   //          //WINNING
-   //          wins = wins + 1;
-   //          $("#wins").html("Wins: " + String(wins));
-   //          resetValues();
-   //      }
-   //  }
-
-   //  	addTotal();
-    
-
-
-
-
-
-});
-
-
-
-
-	
-
-
-
-
-
-
-	// 	var add = [1,2,3,4];
-	// 	var total = 0;
-	// 	for(var i = 0; i < add.length; i++) {
- //   		 total += add[i];
-	// }
-
-
-
-
-
-	
-
-
-
-
-
-
-
-	// ^^^make another on of these for each logo;
-	// then figure out what else happens when the game is over;
-
-
-
-
-
-
-
-	// function newTeams () {
-	// 	var numbers = [];
-	// }
-
-	// function newGame() {
-
-	// }
-
-
-
-	// $('#win').text(wins);
-	// $('#loss').text(losses);
-
-// console.log("Hello from game.js")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var wins = 0;
-// var losses = 0;
-
-// var scorebox = 0;
-
-// var guessingnumberbox = Math.floor((Math.random()* 100)
-// var falconsNumber = Math.floor((Math.random()* 10) + 1);
-// var ugaNumber = Math.floor((Math.random()* 10) + 1);
-// var gaTechNumber = Math.floor((Math.random()* 10) + 1);
-// var hawksNumber = Math.floor((Math.random()* 10) + 1);
-
-
-// var updateScorebox = function (){
-// 	$('.scorebox').empty();
-// 	$('.scorebox').append(add);
-// 	$('#wins').empty();
-// 	$('#wins').append(wins);
-// 	$('#losses').empty();
-// 	$('#losses').append(losses);
-// }
-
-// var restart = function(){
-// 	scorebox = 0;
-//     guessingnumberbox = Math.floor((Math.random()* 100);
-
-// 	$('.scorebox').empty();
-// 	$('.scorebox').append(scorebox);
-
-// 	falconsNumber = Math.floor((Math.random()* 12) + 1);
-//     ugaNumber = Math.floor((Math.random()* 12) + 1);
-//     hawksNumber = Math.floor((Math.random()* 12) + 1);
-//     gaTechNumber = Math.floor((Math.random()* 12) + 1);
-//     //alert('restart');
-//     updateScorebox();
-// }
-// var takingAturn = function (){
-// 	if (scorebox == guessingnumberbox) {
-// 		wins = wins + 1;
-		
-		
-// 		restart();
-// 	}else if(scorebox > guessingnumberbox){
-// 		losses = losses + 1;
-		
-		
-// 		restart();
-// 	}else{
-// 		updateScorebox();
-// 	}
-// }
-// $('.guessingnumberbox').append(scorebox);
-
-// $('.scorebox').append(scorebox);
-// //function
-
-// $(document).ready(function(){
-// 	$('#falcons').click(function(){
-// 		scorebox = scorebox + falconsNumber;
-// 		takingAturn();
-// 	})
-// 	$('#uga').click(function(){
-// 		scorebox = scorebox + ugaNumber;
-// 		takingAturn();
-// 	})
-// 	$('#gaTech').click(function(){
-// 		scorebox = scorebox + gaTechNumber;
-// 		takingAturn();
-// 	})
-// 	$('#hawks').click(function(){
-// 		scorebox = scorebox + hawksNumber;
-// 		takingAturn();
-// 	})
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
